0213f6e2d81ba4ffb8e323ae3d423702663cf4f9
M.S.C. Adolfo Simbron <simbron2803@gmail.com>
2024-10-17 13:42:45 -0600

    Logro: Proyecto Base

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, OnInit, Output, Pipe,} from '@angular/core';
+import { FechaserviceService } from '../../services/fechaservice.service';
+import { DatePipe } from '@angular/common';
+import { log } from 'console';
+
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent   {
+
+currentDate: Date = new Date();
+
+constructor(private fechaService: FechaserviceService){
+  
+}
+  // ngOnInit(): void {
+  //   this.fechaService.dateObs.subscribe(currentDate =>{
+  //     this.currentDate = currentDate;
+     
+  //   });
+
+
+
+  @Output() sideNavToggle=new EventEmitter<boolean>();
+  menuStatus: boolean = false;
+ 
+    SideNavToggle(){
+      this.menuStatus = !this.menuStatus;
+      this.sideNavToggle.emit(this.menuStatus)
+    }
+
+  }
+
+
+    
+  
+
+
+

7afd625085579527d6795e5d01da0bae5f6d047b
ALEXSTR19 <alex129t@outlook.com>
2024-10-23 11:26:11 -0500

    logro: se crea archivo-clinic

---
+++ src/app/business/archivo-clinic/archivo-clinic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archivo-clinic',
+  standalone: true,
+  imports: [],
+  templateUrl: './archivo-clinic.component.html',
+  styleUrl: './archivo-clinic.component.css'
+})
+export class ArchivoClinicComponent {
+
+}
